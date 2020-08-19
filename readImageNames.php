b76fb722b3aeba503d6aadd1f5cf1fd52b88675b
mattias <mattias.hedman@gmail.com>
2020-08-19 07:57:24 +0200

    Ska nu matas in i db...

--- readImageNames.php
+++ readImageNames.php
@@ -1,32 +1,21 @@
 <?php
-$todaydate = date("Y-m-d");
-// image extensions
-$extensions = array('jpg', 'jpeg', 'png');
+    $todaydate = date("Y-m-d");
+    $extensions = array('jpg', 'jpeg', 'png');
 
-// init result
-$result = array();
+    $directory = new DirectoryIterator('img/snurr/');
 
-// directory to scan
-$directory = new DirectoryIterator('img/snurr/');
+    foreach ($directory as $fileinfo) {
 
-// iterate
-foreach ($directory as $fileinfo) {
+        $connect = mysqli_connect('127.0.0.1', 'kravmaga', 'dont-punch-your-friends', 'kms') or die("Failed to connect: " . mysqli_error());
 
-    $connect = mysqli_connect('127.0.0.1', 'kravmaga', 'dont-punch-your-friends', 'kms') or die("Failed to connect: " . mysqli_error());
-
-    // must be a file
-    if ($fileinfo->isFile()) {
-        // file extension
-        $extension = strtolower(pathinfo($fileinfo->getFilename(), PATHINFO_EXTENSION));
-        // check if extension match
-        if (in_array($extension, $extensions)) {
-            // add to result ('".$listarray[0]."', '".$listarray[1]."', '".$listarray[2]."')";
-            $query = "INSERT INTO images (imageName, dateAdded) VALUES ('".$fileinfo->getFilename()."','".$todaydate."' ";
-            echo $query."<br>";
-            //$result[] = $fileinfo->getFilename();
+        if ($fileinfo->isFile()) {
+            $extension = strtolower(pathinfo($fileinfo->getFilename(), PATHINFO_EXTENSION));
+            if (in_array($extension, $extensions)) {
+                $query = "INSERT INTO images (imageName, dateAdded) VALUES ('".$fileinfo->getFilename()."','".$todaydate."' ";
+                if (!mysqli_query($connect, $query)) {
+                    die('An error occurred when submitting. '.mysqli_error());
+                }
+            }
         }
     }
-}
-// print result
-//print_r($result);
 ?>
