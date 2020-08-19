24e422e978a3863dcb5ba7f07c4484e3194086b9
mattias <mattias.hedman@gmail.com>
2020-08-19 08:02:45 +0200

    Försäker fixa felet...

--- readImageNames.php
+++ readImageNames.php
@@ -1,17 +1,16 @@
 <?php
     $todaydate = date("Y-m-d");
-    $extensions = array('jpg', 'jpeg', 'png');
+    $connect = mysqli_connect('127.0.0.1', 'kravmaga', 'dont-punch-your-friends', 'kms') or die("Failed to connect: " . mysqli_error());
 
+    $extensions = array('jpg', 'jpeg', 'png');
     $directory = new DirectoryIterator('img/snurr/');
 
     foreach ($directory as $fileinfo) {
-
-        $connect = mysqli_connect('127.0.0.1', 'kravmaga', 'dont-punch-your-friends', 'kms') or die("Failed to connect: " . mysqli_error());
-
         if ($fileinfo->isFile()) {
             $extension = strtolower(pathinfo($fileinfo->getFilename(), PATHINFO_EXTENSION));
             if (in_array($extension, $extensions)) {
                 $query = "INSERT INTO images (imageName, dateAdded) VALUES ('".$fileinfo->getFilename()."','".$todaydate."' ";
+
                 if (!mysqli_query($connect, $query)) {
                     die('An error occurred when submitting. '.mysqli_error());
                 }
